aeb28e8a22658ee24db7d55d2789136bd08c1887
Yogita Negi <ynegi6@gmail.com>
2023-03-13 23:50:09 +0000

    Added card component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <img alt='robots' src="https://robohash.org/test?200*200 "/>
+        <div>
+            <h2>John Doe</h2>
+            <p>John.doe@gmail.com</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
